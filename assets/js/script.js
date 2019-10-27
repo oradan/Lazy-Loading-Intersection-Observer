d89338519699247da57e230e2be5e9dc0aa44cbc
oradan <plotnica@yahoo.com>
2019-10-27 21:16:50 +0200

    lazy-loading implementation

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+function ImgLazyLoading() {
+    const lazyImgArr = document.querySelectorAll(".lazy-img");
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0
+    };
+
+    function IntersectionCallBack(entries, observer) {
+        entries.forEach(element => {
+            if (element.isIntersecting) {
+                let lazyImg = element.target,
+                    lazyImgParent = lazyImg.parentElement;
+                lazyImg.classList.add("no-visible-c");
+
+                lazyImg.onload = () => {
+                    lazyImgParent.style.height = lazyImg.height + 20 + "px";
+                    lazyImg.classList.add("visible-c");
+                    lazyImg.classList.remove("no-visible-c");
+                }
+                lazyImg.src = lazyImg.dataset.src;
+                lazyImg.classList.remove("lazy-img");
+                observer.unobserve(lazyImg);
+            };
+
+        });
+
+    };
+
+    const observer = new IntersectionObserver(IntersectionCallBack, options)
+    lazyImgArr.forEach(element => {
+        observer.observe(element);
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    ImgLazyLoading();
+});
